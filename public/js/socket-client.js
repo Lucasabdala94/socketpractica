939984da42ece1c69ceab83a74baf3840adb3e02
Lucasabdala94 <lucasabdala94@gmail.com>
2022-05-28 13:07:36 -0300

    comenzando video 8

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,3 +1,19 @@
-
+// Referencia de html
+const lblOnline = document.querySelector('#lblOnline')
+const lblOffline =document.querySelector('#lblOffline')
 
 const socket = io();
+
+
+socket.on('connect',()=>{
+    console.log('conectado');
+    lblOffline.style.display= 'none';
+    lblOnline.style.display='';
+});
+
+socket.on('disconnect',()=>{
+    console.log('Desconecado del servidor');
+    lblOffline.style.display= '';
+    lblOnline.style.display='none';
+})
+
